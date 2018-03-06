9f72a1ba4ceca9823006cdc3406c3eea81b32ae7
nmary01 <nathan.mary6@gmail.com>
2018-03-06 16:32:31 +0100

    Vue

---
+++ MVCTest/src/mvctest/Test.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mvctest;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.table.AbstractTableModel;
+
+/**
+ *
+ * @author Nathan
+ */
+public class Test extends AbstractTableModel {
+    private final List<Eleve> eleves = new ArrayList<Eleve>();
+ 
+    private final String[] entetes = {"Icone", "Nom", "Prenom", "Sexe", "Age"};
+ 
+    public Test() {
+        super();
+ 
+        eleves.add(new Eleve("/resources", "MARY","Nathan",false,20));
+        eleves.add(new Eleve("/resources", "JEAN","Paul", false,22));
+    }
+ 
+    public int getRowCount() {
+        return eleves.size();
+    }
+ 
+    public int getColumnCount() {
+        return entetes.length;
+    }
+ 
+    public String getColumnName(int columnIndex) {
+        return entetes[columnIndex];
+    }
+ 
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        switch(columnIndex){
+            case 0:
+                return eleves.get(rowIndex).getIcone();
+            case 1:
+                return eleves.get(rowIndex).getNom();
+            case 2:
+                return eleves.get(rowIndex).getPrenom();
+            case 3:
+                return eleves.get(rowIndex).isIsfille();
+            case 4:
+                return eleves.get(rowIndex).getAge();
+            default:
+                return null; //Ne devrait jamais arriver
+        }
+    }
+ 
+    public void addEleve(Eleve ami) {
+        eleves.add(ami);
+ 
+        fireTableRowsInserted(eleves.size() -1, eleves.size() -1);
+    }
+ 
+    public void removeEleve(int rowIndex) {
+        eleves.remove(rowIndex);
+ 
+        fireTableRowsDeleted(rowIndex, rowIndex);
+    }
+}
