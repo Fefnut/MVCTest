4d77652f4bb338d03ccba7ded376a87ce05e7cdb
Skaering <paul1995jean@gmail.com>
2018-03-06 16:34:41 +0100

    création des Junits pour les classes exercices , try et reference (il manque que ceux des méthodes avancer ect...)

---
+++ MVCTest/src/mvctest/Vue.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mvctest;
+
+import javax.swing.*;
+
+/**
+ *
+ * @author skaering
+ */
+public class Vue extends JFrame {
+    private JTable tableau;
+    private JPanel tableauZone;
+    private String[] columnNames;
+    private Object[][] data ;
+    private JTable table; 
+public  Vue(){
+
+this.setLocationRelativeTo(null);
+this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+this.setTitle("MVC");
+this.setSize(600,480);
+
+Object[] columns ={"Icone","Nom","Prénom","Sexe","Age"};
+//Object [][] data = fils
+        }
+}
