dbd1463f78f2fb9b26c71cc06d9f62cfdd28f791
nmary01 <nathan.mary6@gmail.com>
2018-03-06 18:09:07 +0100

    Classe formulaire eleve

---
+++ MVCTest/src/mvctest/PanelFormEl.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mvctest;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import javax.swing.*;
+
+/**
+ *
+ * @author Nathan
+ */
+public class PanelFormEl extends JPanel{
+    private JPanel formulaireContent,formulaire;
+    private JTextField fnom,fprenom,fsexe,fage;
+    private JButton save;
+    private Eleve e;
+    
+    public PanelFormEl(){
+        e = new Eleve("/resources", "Paul", "Axelle", "Fille", 19);
+        
+        JLabel lnom=new JLabel("Nom");
+        JLabel lprenom=new JLabel("Prénom");
+        JLabel lsexe=new JLabel("Sexe");
+        JLabel lage=new JLabel("Age");
+        
+        formulaireContent=new JPanel();
+        formulaireContent.setLayout(new GridLayout(5,2));
+        
+        fnom = new JTextField(e.getNom());
+        fprenom = new JTextField(e.getPrenom());
+        fsexe = new JTextField(e.getSexe());
+        fage = new JTextField(Integer.toString(e.getAge()));
+        fnom.setPreferredSize(new Dimension(150,20));
+        fprenom.setPreferredSize(new Dimension(150,20));
+
+        formulaireContent.add(lnom);
+        formulaireContent.add(fnom);
+        formulaireContent.add(lprenom);
+        formulaireContent.add(fprenom);
+        formulaireContent.add(lsexe);
+        formulaireContent.add(fsexe);
+        formulaireContent.add(lage);
+        formulaireContent.add(fage);
+        //this.setContentPane(formulaireContent);
+        
+        save=new JButton("Save changes");
+        
+        //formulaire=new JPanel();
+        this.setLayout(new BorderLayout());
+        this.add(formulaireContent,BorderLayout.CENTER);
+        this.add(save,BorderLayout.SOUTH);
+    }
+}
--- MVCTest/src/mvctest/Vue.java
+++ MVCTest/src/mvctest/Vue.java
@@ -15,51 +15,25 @@ import javax.swing.*;
 public class Vue extends JFrame {
     private Tableau modele = new Tableau();
     private JTable tableau;
-    private JPanel formulaireContent,formulaire;
-    private JTextField fnom,fprenom,fsexe,fage;
-    private JButton save;
-    private Eleve e;
+    private JPanel /*formulaireContent,*/formulaire, rightArea;
+    //private JTextField fnom,fprenom,fsexe,fage;
+    //private JButton save;
+    //private Eleve e;
  
     public Vue() {
         super();
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        //tableau = new JTable(modele);
+        this.setResizable(true);
+        
+        rightArea=new JPanel();
+        rightArea.setLayout(new BorderLayout());
+        
+        tableau = new JTable(modele);
+        formulaire =new PanelFormEl();
         //this.getContentPane().add(new JScrollPane(tableau), BorderLayout.CENTER);
-        
-        e = new Eleve("/resources", "Paul", "Axelle", "Fille", 19);
-        
-        JLabel lnom=new JLabel("Nom");
-        JLabel lprenom=new JLabel("Prénom");
-        JLabel lsexe=new JLabel("Sexe");
-        JLabel lage=new JLabel("Age");
-        
-        formulaireContent=new JPanel();
-        formulaireContent.setLayout(new GridLayout(5,2));
-        
-        fnom = new JTextField(e.getNom());
-        fprenom = new JTextField(e.getPrenom());
-        fsexe = new JTextField(e.getSexe());
-        fage = new JTextField(Integer.toString(e.getAge()));
-        fnom.setPreferredSize(new Dimension(150,20));
-        fprenom.setPreferredSize(new Dimension(150,20));
-
-        formulaireContent.add(lnom);
-        formulaireContent.add(fnom);
-        formulaireContent.add(lprenom);
-        formulaireContent.add(fprenom);
-        formulaireContent.add(lsexe);
-        formulaireContent.add(fsexe);
-        formulaireContent.add(lage);
-        formulaireContent.add(fage);
-        this.setContentPane(formulaireContent);
-        
-        save=new JButton("Save changes");
-        
-        formulaire=new JPanel();
-        formulaire.setLayout(new BorderLayout());
-        formulaire.add(formulaireContent,BorderLayout.CENTER);
-        formulaire.add(save,BorderLayout.SOUTH);
-        this.setContentPane(formulaire);
+        rightArea.add(tableau,BorderLayout.SOUTH);
+        rightArea.add(formulaire,BorderLayout.CENTER);
+        this.setContentPane(rightArea);
         
         this.pack();
         this.setVisible(true);
