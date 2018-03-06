6f65e002bf21e04ae30962a8bc7cc648e2cb1674
nmary01 <nathan.mary6@gmail.com>
2018-03-06 15:18:11 +0100

    Implem Eleve

--- MVCTest/src/mvctest/Eleve.java
+++ MVCTest/src/mvctest/Eleve.java
@@ -10,5 +10,58 @@ package mvctest;
  * @author Nathan
  */
 public class Eleve {
+    private String icone,nom,prenom;
+    private boolean isfille;
+    private int age;
+
+    public Eleve(String icone, String nom, String prenom, boolean isfille, int age) {
+        this.icone = icone;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.isfille = isfille;
+        this.age = age;
+    }
+
+    public String getIcone() {
+        return icone;
+    }
+
+    public void setIcone(String icone) {
+        this.icone = icone;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public boolean isIsfille() {
+        return isfille;
+    }
+
+    public void setIsfille(boolean isfille) {
+        this.isfille = isfille;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+    
+    
     
 }
